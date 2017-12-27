8b9db134e7e8c8908a4662ca850bb12230d67d1d
Michael <mderazon@gmail.com>
2017-12-27 14:54:43 +0200

    update formatting

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# node-fpe [![Build Status](https://travis-ci.org/mderazon/node-fpe.svg?branch=master)](https://travis-ci.org/mderazon/node-fpe) [![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+# node-fpe [![Build Status](https://travis-ci.org/mderazon/node-fpe.svg?branch=master)](https://travis-ci.org/mderazon/node-fpe) [![styled with prettier](https://img.shields.io/badge/styled_with-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
 
-
-> Simple format-preserving encryprion implementation
-
+> Simple format-preserving encryption implementation
 
 In general, [format-preserving encryption](https://en.wikipedia.org/wiki/Format-preserving_encryption) is a type of encryption such that the output (the ciphertext) is in the same format as the input (the plaintext).
 
@@ -11,35 +9,37 @@ This library uses a simple FPE from a [prefix cipher](https://en.wikipedia.org/w
 ## Usage
 
 ### Example:
+
 cipher with default domain ([0-9]) and default encryption alg (aes-256-cbc):
 
 ```js
-const fpe = require('node-fpe')
-const cipher = fpe({password: 'secret'})
+const fpe = require('node-fpe');
+const cipher = fpe({ password: 'secret' });
 
-cipher.encrypt('1234567')
+cipher.encrypt('1234567');
 // '4185730'
 
-cipher.decrypt('4185730')
+cipher.decrypt('4185730');
 // '1234567'
 ```
 
 cipher with custom domain ([A-E]) and default encryption alg (aes-256-cbc):
 
 ```js
-const fpe = require('node-fpe')
-const cipher = fpe({password: 'secret', domain: ['A', 'B', 'C', 'D', 'E']})
+const fpe = require('node-fpe');
+const cipher = fpe({ password: 'secret', domain: ['A', 'B', 'C', 'D', 'E'] });
 
-cipher.encrypt('BEEBEE')
+cipher.encrypt('BEEBEE');
 // 'CBBCBB'
 
-cipher.decrypt('CBBCBB')
+cipher.decrypt('CBBCBB');
 // 'BEEBEE'
 ```
 
 ### Options
-Options to pass on to *node-fpe* are:
-- `password`: **mandatory**. a secret used in the underlying block cipher.
-- `algorithm`: **optional**. the underlying block cipher used. similar to the input to node's [crypto.createCipher()](https://nodejs.org/api/crypto.html#crypto_crypto_createcipher_algorithm_password). **default**: *aes-256-cbc*
-- `domain`: **optional**. an array of characters used as the FPE domain. **default**: 0-9 digits
 
+Options to pass on to _node-fpe_ are:
+
+* `password`: **mandatory**. a secret used in the underlying block cipher.
+* `algorithm`: **optional**. the underlying block cipher used. similar to the input to node's [crypto.createCipher()](https://nodejs.org/api/crypto.html#crypto_crypto_createcipher_algorithm_password). **default**: _aes-256-cbc_
+* `domain`: **optional**. an array of characters used as the FPE domain. **default**: 0-9 digits
--- test/index.js
+++ test/index.js
@@ -1,46 +1,56 @@
-const test = require('tape')
-const fpe = require('../lib')
+const test = require('tape');
+const fpe = require('../lib');
 
-test('missing password', (t) => {
-  t.plan(1)
-  t.throws(() => fpe())
-})
+test('missing password', t => {
+  t.plan(1);
+  t.throws(() => fpe());
+});
 
-test('encrypt-decrypt', (t) => {
-  t.plan(1)
-  const cipher = fpe({ password: 'secret' })
-  t.equal(cipher.decrypt(cipher.encrypt('12345')), '12345')
-})
+test('encrypt-decrypt', t => {
+  t.plan(1);
+  const cipher = fpe({ password: 'secret' });
+  t.equal(cipher.decrypt(cipher.encrypt('12345')), '12345');
+});
 
-test('different passwords', (t) => {
-  t.plan(1)
-  const cipher1 = fpe({ password: 'secret' })
-  const cipher2 = fpe({ password: 'other secret' })
-  t.notEqual(cipher1.encrypt('12345'), cipher2.encrypt('12345'))
-})
+test('different passwords', t => {
+  t.plan(1);
+  const cipher1 = fpe({ password: 'secret' });
+  const cipher2 = fpe({ password: 'other secret' });
+  t.notEqual(cipher1.encrypt('12345'), cipher2.encrypt('12345'));
+});
 
-test('different domain', (t) => {
-  t.plan(1)
-  const cipher = fpe({ password: 'secret', domain: ['A', 'B', 'C', 'D', 'E'] })
-  t.equal(cipher.decrypt(cipher.encrypt('ABBA')), 'ABBA')
-})
+test('ascii domain', t => {
+  var ascii = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'.split(
+    ''
+  );
+  t.plan(1);
+  const cipher = fpe({ password: 'secret', domain: ascii });
+  console.log(cipher.encrypt('ABBA'));
+  t.equal(cipher.decrypt(cipher.encrypt('ABBA')), 'ABBA');
+});
 
-test('characters not in domain', (t) => {
-  t.plan(2)
-  const cipher = fpe({ password: 'secret', domain: ['A', 'B', 'C', 'D', 'E'] })
-  t.throws(() => cipher.encrypt('ABF'))
-  t.throws(() => cipher.decrypt('ABF'))
-})
+test('different domain', t => {
+  t.plan(1);
+  const cipher = fpe({ password: 'secret', domain: ['A', 'B', 'C', 'D', 'E'] });
+  t.equal(cipher.decrypt(cipher.encrypt('ABBA')), 'ABBA');
+});
 
-test('different algorithm', (t) => {
-  t.plan(1)
-  const cipher = fpe({ password: 'secret', algorithm: 'aes192' })
-  t.equal(cipher.decrypt(cipher.encrypt('12345')), '12345')
-})
+test('characters not in domain', t => {
+  t.plan(2);
+  const cipher = fpe({ password: 'secret', domain: ['A', 'B', 'C', 'D', 'E'] });
+  t.throws(() => cipher.encrypt('ABF'));
+  t.throws(() => cipher.decrypt('ABF'));
+});
 
-test('input not a string', (t) => {
-  t.plan(2)
-  const cipher = fpe({ password: 'secret' })
-  t.throws(() => cipher.encrypt(123))
-  t.throws(() => cipher.decrypt(123))
-})
+test('different algorithm', t => {
+  t.plan(1);
+  const cipher = fpe({ password: 'secret', algorithm: 'aes192' });
+  t.equal(cipher.decrypt(cipher.encrypt('12345')), '12345');
+});
+
+test('input not a string', t => {
+  t.plan(2);
+  const cipher = fpe({ password: 'secret' });
+  t.throws(() => cipher.encrypt(123));
+  t.throws(() => cipher.decrypt(123));
+});
